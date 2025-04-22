07cfd66ec88fa0d337f2705b979ea3934749bed4
Manojkumar2110 <harimanoj1991@gmail.com>
2025-04-22 11:07:43 +0530

    updates in checkout page

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js'
+import {cart, addToCart,calculateCartQuantity} from '../data/cart.js'
 import {products} from '../data/products.js'
 import {formatCurrency} from './utils/money.js';
 let productsHTML='';
@@ -58,14 +58,11 @@ products.forEach((product)=>{//Accumulator pattern:  loop through array and each
 document.querySelector('.js-products-grid').innerHTML=productsHTML;
 
 function updateCartQuantity(){
-  let cartQuantity=0;
-  cart.forEach((cartItem)=>{
-    cartQuantity+=cartItem.quantity;
-  });
+  const cartQuantity = calculateCartQuantity();
 
   document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
 }
-
+updateCartQuantity();
 
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=>{
